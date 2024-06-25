cbb556aa5310de7c3e187327860cda6efe84f1a4
M.Attef <mahmoud.elgmmal@PI-DEV-05>
2024-06-25 16:07:15 +0300

    add component in component.html

--- temp-project-master/src/app/app.component.ts
+++ temp-project-master/src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SinginComponent } from './components/singin/singin.component';
+import { ActivityComponent } from './components/activity/activity.component';
+import { ActivityTitleComponent } from './components/activity-title/activity-title.component';
+import { RankingOfPlayerComponent } from './components/ranking-of-player/ranking-of-player.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SinginComponent],
+  imports: [RouterOutlet,SinginComponent,ActivityComponent,ActivityTitleComponent,RankingOfPlayerComponent,RewardsComponent,ProfileComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
--- temp-project-master/src/app/app.module.ts
+++ temp-project-master/src/app/app.module.ts
@@ -1,9 +1,21 @@
+import { RankingOfPlayerComponent } from './components/ranking-of-player/ranking-of-player.component';
+import { RewardsComponent } from './components/rewards/rewards.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { ActivityTitleComponent } from './components/activity-title/activity-title.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import {SinginComponent} from './components/singin/singin.component';
+import { ActivityComponent } from './components/activity/activity.component';
+
+
+
+
+
+
 
 
 
@@ -17,6 +29,12 @@ import {SinginComponent} from './components/singin/singin.component';
     FormsModule,
     SinginComponent,
     AppComponent ,
+    ActivityComponent,
+    ActivityTitleComponent,
+    HomeComponent,
+    ProfileComponent,
+    RewardsComponent,
+    RankingOfPlayerComponent,
     HttpClientModule
     // Add other modules here
   ],
