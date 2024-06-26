91a778df2beded6ae1a47fc1aba9a0179f46f2ad
M.Attef <mahmoud.elgmmal@PI-DEV-05>
2024-06-26 15:43:57 +0300

    add routing

--- temp-project-master/src/app/app.routes.ts
+++ temp-project-master/src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+//import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//export const routes: Routes = [];
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: 'tasks', component: HomeComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' } // Default route
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
